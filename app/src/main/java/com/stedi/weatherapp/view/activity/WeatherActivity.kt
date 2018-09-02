d6aa1a3dc3497e33cb85a2fdafed291ee63a870e
Dima Stepanchenko <stepanchenkodmitriy@gmail.com>
2018-09-02 12:25:27 +0200

    showing better error messages in weather activity

--- app/src/main/java/com/stedi/weatherapp/view/activity/WeatherActivity.kt
+++ app/src/main/java/com/stedi/weatherapp/view/activity/WeatherActivity.kt
@@ -14,7 +14,6 @@ import com.stedi.weatherapp.R
 import com.stedi.weatherapp.model.data.owmweather.CityWeather
 import com.stedi.weatherapp.other.getApp
 import com.stedi.weatherapp.other.getInternalDrawableFromOWMIcon
-import com.stedi.weatherapp.other.showToastLong
 import com.stedi.weatherapp.presenter.interfaces.WeatherPresenter
 import com.stedi.weatherapp.view.components.BaseViewModel
 import javax.inject.Inject
@@ -36,6 +35,12 @@ class WeatherActivity : AppCompatActivity(), WeatherPresenter.UIImpl {
 
     private val CITY_SEARCH_REQUEST_CODE = 111
 
+    enum class WeatherError {
+        NO_SELECTED_CITY,
+        NO_DATA_FOR_CITY,
+        INTERNAL
+    }
+
     private lateinit var viewModel: WeatherActivityModel
 
     @BindView(R.id.weather_activity_iv_weather) lateinit var ivWeather: ImageView
@@ -46,6 +51,7 @@ class WeatherActivity : AppCompatActivity(), WeatherPresenter.UIImpl {
 
     private var cityWeather: CityWeather? = null
     private var cityName: String? = null
+    private var weatherError: WeatherError? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -84,43 +90,61 @@ class WeatherActivity : AppCompatActivity(), WeatherPresenter.UIImpl {
     override fun showWeather(cityWeather: CityWeather) {
         this.cityWeather = cityWeather
         this.cityName = cityWeather.name
+        this.weatherError = null
         invalidate()
     }
 
     override fun showNoSelectedCity() {
         cityWeather = null
         cityName = null
+        weatherError = WeatherError.NO_SELECTED_CITY
         invalidate()
     }
 
     override fun showFailedToGetWeather(forCity: String?) {
         cityWeather = null
         cityName = forCity
-        if (forCity != null) {
-            showToastLong(getString(R.string.no_weather_for, forCity))
+        weatherError = if (forCity != null) {
+            WeatherError.NO_DATA_FOR_CITY
         } else {
-            showToastLong(R.string.failed_to_get_weather)
+            WeatherError.INTERNAL
         }
         invalidate()
     }
 
     private fun invalidate() {
         val unknownValue = "??"
-
-        val weather = cityWeather?.weather?.firstOrNull()
-        val icon = weather?.icon ?: unknownValue
-        val temperature = cityWeather?.main?.temp ?: unknownValue
-        val description = weather?.description ?: unknownValue
-        val pressure = cityWeather?.main?.pressure ?: unknownValue
-        val humidity = cityWeather?.main?.humidity ?: unknownValue
-        val wind = cityWeather?.wind?.speed ?: unknownValue
-        val cloudiness = cityWeather?.clouds?.all ?: unknownValue
-        val cityName = cityName ?: getString(R.string.no_selected_city)
-
-        ivWeather.setImageResource(getInternalDrawableFromOWMIcon(icon))
-        tvTemperature.text = getString(R.string.temperature_celsius, temperature)
-        tvDescription.text = getString(R.string.weather_description, description)
-        tvMoreWeatherInfo.text = getString(R.string.weather_more_info, cloudiness, pressure, humidity, wind)
-        tvCityName.text = cityName
+        when (weatherError) {
+            null -> {
+                val weather = cityWeather?.weather?.firstOrNull()
+                val icon = weather?.icon ?: unknownValue
+                val temperature = cityWeather?.main?.temp ?: unknownValue
+                val description = weather?.description ?: unknownValue
+                val pressure = cityWeather?.main?.pressure ?: unknownValue
+                val humidity = cityWeather?.main?.humidity ?: unknownValue
+                val wind = cityWeather?.wind?.speed ?: unknownValue
+                val cloudiness = cityWeather?.clouds?.all ?: unknownValue
+                val cityName = cityName ?: unknownValue
+                ivWeather.setImageResource(getInternalDrawableFromOWMIcon(icon))
+                tvTemperature.text = getString(R.string.temperature_celsius, temperature)
+                tvDescription.text = getString(R.string.weather_description, description)
+                tvMoreWeatherInfo.text = getString(R.string.weather_more_info, cloudiness, pressure, humidity, wind)
+                tvCityName.text = cityName
+            }
+            WeatherError.NO_SELECTED_CITY -> {
+                ivWeather.setImageResource(R.drawable.ic_cloud_search)
+                tvTemperature.text = getString(R.string.temperature_celsius, unknownValue)
+                tvDescription.text = getString(R.string.no_selected_city)
+                tvMoreWeatherInfo.text = ""
+                tvCityName.text = getString(R.string.no_selected_city)
+            }
+            else -> {
+                ivWeather.setImageResource(R.drawable.ic_cloud_question)
+                tvTemperature.text = getString(R.string.temperature_celsius, unknownValue)
+                tvDescription.text = cityName?.let { getString(R.string.no_weather_for, it) } ?: getString(R.string.failed_to_get_weather)
+                tvMoreWeatherInfo.text = ""
+                tvCityName.text = cityName ?: getString(R.string.no_selected_city)
+            }
+        }
     }
 }
